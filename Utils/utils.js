74389caf8f7ad697fe58544aa59ee298a5b55945
Ollie Jones <oathompsonjones@gmail.com>
2021-06-04 19:57:47 +0100

    Update utils.js

--- Utils/utils.js
+++ Utils/utils.js
@@ -1,10 +1,17 @@
 const range = (start, end) => Array(end - start + 1).fill(0).map((_, idx) => start + idx);
+const map = (x, inMin, inMax, outMin, outMax) => ((x - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+
+const factorial = (n) => Array(n).fill(0).map((_, i) => i + 1).reduce((a, b) => a * b);
+const pentagonal = (n) => n * (3 * n - 1) / 2;
+
 const isPrime = (n) => {
-    for (let i = 2; i <= Math.sqrt(n); i++) if (n % i === 0) return false;
+    for (let i = 2; i <= Math.sqrt(n); i++)
+        if (n % i === 0)
+            return false;
     return n > 1;
 };
 const isPandigital = (n) => Array(Math.min(n.toString().length, 9)).fill(0).map((_, i) => i + 1).every((d) => n.toString().includes(d));
-const factorial = (n) => Array(n).fill(0).map((_, i) => i + 1).reduce((a, b) => a * b);
+
 const swap = (array1, index1, index2) => {
     let temp;
     temp = array1[ index1 ];
@@ -17,17 +24,23 @@ const permutations = (a) => {
     const output = [];
     const permute = (a, l, r) => {
         if (l == r) output.push(a.join(""));
-        else {
-            for (let i = l; i <= r; i++) {
-                swap(a, l, i);
-                permute(a, l + 1, r);
-                swap(a, l, i);
-            }
+        else for (let i = l; i <= r; i++) {
+            swap(a, l, i);
+            permute(a, l + 1, r);
+            swap(a, l, i);
         }
     };
     permute(a, l, r);
     return output;
 };
-const pentagonal = (n) => n * (3 * n - 1) / 2;
 
-module.exports = { range, isPrime, isPandigital, factorial, swap, permutations, pentagonal };
+module.exports = {
+    range,
+    map,
+    factorial,
+    pentagonal,
+    isPrime,
+    isPandigital,
+    swap,
+    permutations
+};
