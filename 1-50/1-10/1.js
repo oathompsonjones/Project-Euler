f3c39fd6357a145ca16bc562f88e7d18a6876214
Ollie Jones <oathompsonjones@gmail.com>
2022-04-05 20:29:57 +0100

    Redo first problem

--- 1-50/1-10/1.js
+++ 1-50/1-10/1.js
@@ -1,15 +1,5 @@
-let loop = 1;
-let numArr = [];
-let total = 0;
-while (loop < 1000) {
-    if ((loop % 3 === 0) || (loop % 5 === 0)) {
-        numArr.push(loop);
-    }
-    loop++;
-}
-
-numArr.forEach(num => {
-    total += num;
-});
-
-console.log(total);
+let sum = 0;
+for (let i = 1; i < 1000; i++)
+    if (i % 3 === 0 || i % 5 === 0)
+        sum += i;
+console.log(sum);
