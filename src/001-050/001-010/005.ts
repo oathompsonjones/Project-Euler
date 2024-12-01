d51299e27a7e5d91eec280a7e119811598f2e65c
Ollie Jones <oathompsonjones@gmail.com>
2024-12-01 17:54:02 +0000

    Make problem 5 about 15x faster

--- src/001-050/001-010/005.ts
+++ src/001-050/001-010/005.ts
@@ -1,32 +1,18 @@
-let num = 1;
 let result = NaN;
 
-while (isNaN(result)) {
+for (let num = 20; isNaN(result); num += 20) {
     if (
-        num % 1 === 0 &&
-        num % 2 === 0 &&
-        num % 3 === 0 &&
-        num % 4 === 0 &&
-        num % 5 === 0 &&
-        num % 6 === 0 &&
-        num % 7 === 0 &&
-        num % 8 === 0 &&
-        num % 9 === 0 &&
-        num % 10 === 0 &&
-        num % 11 === 0 &&
-        num % 12 === 0 &&
-        num % 13 === 0 &&
-        num % 14 === 0 &&
-        num % 15 === 0 &&
-        num % 16 === 0 &&
-        num % 17 === 0 &&
-        num % 18 === 0 &&
         num % 19 === 0 &&
-        num % 20 === 0
+        num % 18 === 0 &&
+        num % 17 === 0 &&
+        num % 16 === 0 &&
+        num % 15 === 0 &&
+        num % 14 === 0 &&
+        num % 13 === 0 &&
+        num % 12 === 0 &&
+        num % 11 === 0
     )
         result = num;
-
-    num++;
 }
 
 export default result;
